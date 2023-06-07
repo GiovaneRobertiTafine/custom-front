32dbd68f7eacd4e6ea35053ad3ec1a7f1687672b
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2023-06-07 16:44:24 -0300

    Inserindo change fonte

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 export class Main {
     private swapper = document.querySelector<HTMLElement>('.button-swapper')!;
     private altoContraste = document.querySelector<HTMLElement>('.button-alto-contraste')!;
+    private fonteMais = document.querySelector<HTMLElement>('#btn-fonte-mais')!;
+    private fonteMenos = document.querySelector<HTMLElement>('#btn-fonte-menos')!;
     private lastMode = '';
 
     constructor() {
@@ -19,6 +21,8 @@ export class Main {
     }
 
     modeViewEventListener(): void {
+        this.fontSizeBody = '100';
+        this.changeFonte();
         this.swapper.addEventListener(
             'click', () => {
                 console.log(document.documentElement.getAttribute('data-theme'));
@@ -39,6 +43,7 @@ export class Main {
                 localStorage.setItem('data-swapper-theme', document.documentElement.getAttribute('data-swapper-theme')!);
                 localStorage.setItem('data-swapper-rotate', document.documentElement.getAttribute('data-swapper-rotate')!);
                 this.lastMode = '';
+                this.changeFonte();
             }
         );
         this.altoContraste.addEventListener(
@@ -51,15 +56,43 @@ export class Main {
                 }
                 localStorage.setItem('data-theme', document.documentElement.getAttribute('data-theme')!);
                 localStorage.setItem('data-swapper-theme', document.documentElement.getAttribute('data-swapper-theme')!);
+                this.changeFonte();
             }
         );
-
+        this.fonteMais.addEventListener(
+            'click', () => {
+                this.changeFonte('mais');
+            }
+        );
+        this.fonteMenos.addEventListener(
+            'click', () => {
+                this.changeFonte('menos');
+            }
+        );
     }
 
-    setTheme(): void {
-
+    changeFonte(param?: 'mais' | 'menos'): void {
+        if (param) {
+            if (param === 'mais' && this.fontSizeBody !== '24px') {
+                this.fontSizeBody = (+this.fontSizeBody.substring(0, 2) + 4) + 'px';
+            } else if (param === 'menos' && this.fontSizeBody !== '16px') {
+                this.fontSizeBody = (+this.fontSizeBody.substring(0, 2) - 4) + 'px';
+            }
+        }
+        if (this.fontSizeBody === '16px') {
+            (this.fonteMenos as HTMLBodyElement).style.color = getComputedStyle(document.body).getPropertyValue('--font-disabled');
+        } else {
+            (this.fonteMenos as HTMLBodyElement).style.color = getComputedStyle(document.body).getPropertyValue('--font-color');
+        };
+        if (this.fontSizeBody === '24px') {
+            (this.fonteMais as HTMLBodyElement).style.color = getComputedStyle(document.body).getPropertyValue('--font-disabled');
+        } else {
+            (this.fonteMais as HTMLBodyElement).style.color = getComputedStyle(document.body).getPropertyValue('--font-color');
+        };
     }
 
+    get fontSizeBody(): string { return getComputedStyle(document.querySelector('body')!).fontSize; }
+    set fontSizeBody(newValue: string) { (document.querySelector('body') as HTMLBodyElement).style.fontSize = newValue; }
 }
 
 new Main();
